33db0211f94c17134bc5c358bb2a62bf9aa10807
Andrew Martins <andrew_omartins@yahoo.com>
2022-09-10 03:06:34 -0400

    working with control statements in python

---
+++ controlStatements.py
@@ -0,0 +1,14 @@
+# bonus = 20
+# if bonus <= 10:
+#     bonus = 100
+# else:
+#     bonus = 200
+# print ("\n bonus = ", bonus)
+
+age = int(input("How old is you?: "))
+if age < 18:
+    print("You are not eligible to vote")
+else:
+    print("You are eligible to vote") 
+    
+
