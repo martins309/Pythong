74b00a1b998aa0b12e8ce71e675b414984a8d975
Andrew Martins <andrew_omartins@yahoo.com>
2023-05-07 00:15:43 -0400

    finished the game now testing for bugs

--- games/hangman.py
+++ games/hangman.py
@@ -1,7 +1,8 @@
+
 import time 
 
 name = input("What is your name?")
-print("hello", + name, "Its show time")
+print("hello," + name, "Its show time")
 
 time.sleep(1)
 
@@ -11,11 +12,26 @@ time.sleep(0.5)
 word = "chicken"
 
 guesses = ''
+turns = 10
 
-while guesses > 0:
+while turns > 0:
     failed = 0
-    for char in guesses:
-        print(char, end=""),
-    else:
-        print("_",end="")
-    failed += 1
+    for char in word:
+        if char in guesses:
+            print(char, end="")
+        else:
+            print("_",end="")
+            failed += 1
+    if failed == 0:
+        print("you won!!")
+        break
+    guess = input(" guess a character:")
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print("that is incorrect")
+        print("you have,", turns, "more guesses")
+
+        if turns ==  0:
+            print("you suck")
