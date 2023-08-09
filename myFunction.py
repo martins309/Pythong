4b3867085aa1d047d18d5b1928b3f24afcb68037
Andrew Martins <andrew_omartins@yahoo.com>
2023-08-08 23:33:38 -0400

    created a function, and I want to implement randit

---
+++ myFunction.py
@@ -0,0 +1,21 @@
+#Creating different functions for practice
+
+#figure out how to use the rand it module
+
+def myFunction():
+    x = 6
+    y = 9
+    array = [3,6,9,2,21,22]
+    for i in array:
+        if x % 2 == 0:
+            print("This is a number in the index:", array[i])
+        elif y % 2 == 0:
+            print(array[i ** 2])
+        else:
+            array[i -8 *1000]
+
+        
+
+
+
+    myFunction()
