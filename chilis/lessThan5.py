8c27d606ce87ff1d7c4cb73c6965f23bec176c76
Andrew Martins <andrew_omartins@yahoo.com>
2023-10-14 20:39:08 -0400

    wrote programn to find numbers that are less than five or less than five

---
+++ chilis/lessThan5.py
@@ -0,0 +1,20 @@
+# Take a list, say for example this one:
+
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# Extras:
+
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+
+def lessThan5(array):
+    for x in array:
+        if x > 5:
+            print("this number is too big")
+        else:
+            print(x)
+
+lessThan5( [1, 3, 6, 5, 4, 33, 342, 3, 64, 4])
