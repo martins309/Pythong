cc8ac8374f093edabf4d26dee4f50a107740ce8c
Andrew Martins <andrew_omartins@yahoo.com>
2023-11-04 00:00:38 -0400

    new logic to find out how many divisors exist within a list

--- chilis/divisors.py
+++ chilis/divisors.py
@@ -2,3 +2,16 @@
 # and then prints out a list of all the divisors of that number. 
 # (If you don’t know what a divisor is, it is a number that divides evenly into another number. 
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+
+ def divisor():
+     div = []
+     y = range(10,28)
+     num = int(input("gimme a number little boy:  "))
+     print()
+     __=input("and make it snappy or else")
+     
+     for el in y:
+         if num % y == 0:
+             div.append(num)
+    print(div)
