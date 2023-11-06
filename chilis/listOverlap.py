9ce03a6ca1ce46733bbd8b36a22e3b7cf2b88127
Andrew Martins Jr. <75143566+martins309@users.noreply.github.com>
2023-11-06 04:33:19 +0000

    commented the parameters of the program

---
+++ chilis/listOverlap.py
@@ -0,0 +1,6 @@
+#write a program that returns a list of the most common elements without returning the duplicates
+#program must work on two lists that are of different size 
+#for extras:
+#randomly generete two lists to test this and write it one line
+
+
