4a85c2a6565e657c11e41171c8d8f6f3f47e3fe2
Andrew Martins <andrew_omartins@yahoo.com>
2023-11-06 23:27:13 -0500

    testing current solution

--- chilis/listOverlap.py
+++ chilis/listOverlap.py
@@ -9,6 +9,6 @@ def listOverlap():
     list2 = [1,1,1,1,2,3,4,5,6,7,8,8,8]
     
     newList = []
-    
-    for i in zip_longest(list1, list2):
-        
+        for i in list1 for j in list2:
+            print(set(list(i,j)))
+listOverlap()
